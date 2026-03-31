f0a7c06e25ea375672047ce691e99ac2ff427b22
suramashaqi <s12323571@stu.najah.edu>
2026-03-31 23:10:17 +0300

    m

--- web2/src/components/Navbar.tsx
+++ web2/src/components/Navbar.tsx
@@ -1,8 +1,9 @@
 export default function Navbar() {
   return (
-    <div className="flex justify-between items-center px-8 py-4 bg-[#1e1b4b] text-white">
+     <div className="flex justify-between items-center px-8 py-4 bg-[#1e1b4b] text-white">
 
       {/* Logo */}
+      
       <h1 className="text-xl font-bold">
         <span className="text-indigo-400">ttwar</span> تتطور
       </h1>
